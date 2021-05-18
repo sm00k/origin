de344598a0eadd78192e1cd3d92c4a2a36c90919
Sergey <sm00ck1314@gmail.com>
2021-05-18 22:57:33 +0300

    add function comparison

--- comparison_of_numbers/main.cpp
+++ comparison_of_numbers/main.cpp
@@ -3,11 +3,50 @@
 
 using namespace std;
 
-
-
-void separation (string num, string &left, string &right)
+void verification_right (string one_right_half, string two_right_half)
 {
-    bool dot = false;
+    int num_one_right = stoi(one_right_half), num_two_right = stoi(two_right_half);
+    if (num_one_right > num_two_right)
+    {
+       cout << "More\n";
+    }
+    else if (num_one_right < num_two_right)
+    {
+       cout << "less\n";
+    }
+    else
+    {
+        cout << "equal\n";
+    }
+}
+
+void verification_left (string one_left_half, string two_left_half )
+{
+    int num_one = stoi(one_left_half), num_two = stoi(two_left_half);
+    if (num_one > num_two)
+    {
+        cout << "More\n";
+    }
+    else if (num_one < num_two)
+    {
+        cout << "less\n";
+    }
+    else
+    {
+        if (dot)
+        {
+           cout << "equal\n";
+        }
+        else
+        {
+            // переход в следующую функцию.
+        }
+
+    }
+}
+
+void separation (string num, string &left, string &right, bool dot)
+{
     for (int i = 0;!dot && i < (int)num.size(); ++i)
     {
         if (num[i] != '.')
@@ -65,7 +104,7 @@ void user_enter (string &num, string left, string right)
 
     if ((int_end - int_start) + (num.length() - frac_start) == 0) ok = false;
 
-    ok ? separation(num, left, right) : throw (-1);   //передача в следующую функцию
+    ok ? separation(num, left, right, dot) : throw (-1);   //передача в следующую функцию
 }
 
 int main ()
